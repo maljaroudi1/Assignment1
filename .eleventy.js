1218b079f1227790a80477100c0535ac2540991a
maljaroudi1 <meojax.ajx@gmail.com>
2023-09-27 14:54:56 -0400

    new files

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("static");
+  
+    return {
+      dir: {
+        input: "src",
+        output: "dist",
+      },
+    };
+  };
+  
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+// 301269584
+// Mohammed Aljaroudi
+// 9/27/2023
+// index.js
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res) {
+  res.render('projects', { title: 'Project' });
+});
+
+
+router.get('/services', (req, res) => {
+  res.render('services', { title: 'Services' });
+});
+
+
+module.exports = router;
